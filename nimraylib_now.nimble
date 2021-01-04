43216e3161952c88eff6efae81d7dcf55f50826e
Dmitry Matveyev <dm.matveyev@protonmail.com>
2021-01-04 18:29:53 +0600

    Add script for downloading files

--- nimraylib_now.nimble
+++ nimraylib_now.nimble
@@ -6,12 +6,11 @@ description = "Raylib wrapper for head version"
 license = "MIT"
 srcDir = "src"
 bin = @["nimraylib_now"]
-backend = "js"
+backend = "c"
 
 # Dependencies
 
 requires "nim >= 1.4.2"
 
 task dorun, "run":
-  exec "nim js -d:nodej -o:nimraylib_now.js src/nimraylib_now.nim"
-  exec "node " & bin[0]
+  exec "nim r src/nimraylib_now"
--- src/nimraylib_now.nim
+++ src/nimraylib_now.nim
@@ -1,5 +1,38 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import httpclient, asyncdispatch
+from os import `/`, fileExists, extractFilename
+from strutils import split
 
-when isMainModule:
-  echo("Hello, World!")
+const
+  downloadDirectory = "raylib"
+  urlStart = "https://raw.githubusercontent.com/raysan5/"
+  filesToDownload = [
+    "raylib/master/src/raylib.h",
+    "raygui/master/src/raygui.h",
+    "raylib/master/src/rlgl.h",
+    "raylib/master/src/raymath.h",
+  ]
+
+
+# Download header files
+
+proc downloadSources() {.async.} =
+  var futures: seq[Future[void]]
+  for file in filesToDownload:
+    var client = newAsyncHttpClient()
+    let
+      url = urlStart & file
+      targetPath = downloadDirectory/file.extractFilename
+    futures.add client.downloadFile(url, targetPath)
+  for future in futures:
+    await future
+
+var doDownload = false
+for file in filesToDownload:
+  if not fileExists(downloadDirectory/file.extractFilename):
+    doDownload = true
+    break
+if doDownload:
+  waitFor downloadSources()
+
+
+# Parse files to (((Nim))) wrappers
