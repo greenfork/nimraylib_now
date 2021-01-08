4780f353e287d3444528913b0c3d4e085b1a2b8f
Dmitry Matveyev <dm.matveyev@protonmail.com>
2021-01-08 20:35:59 +0600

    Add core_scissor_test.nim example

---
+++ examples/core/core_scissor_test.nim
@@ -0,0 +1,55 @@
+# ******************************************************************************************
+#
+#    raylib [core] example - Scissor test
+#
+#    This example has been created using raylib 2.5 (www.raylib.com)
+#    raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+#
+#    Example contributed by Chris Dill (@MysteriousSpace) and reviewed by Ramon Santamaria (@raysan5)
+#
+#    Copyright (c) 2019 Chris Dill (@MysteriousSpace)
+#    Converted in 2021 by greenfork
+#
+# ******************************************************************************************
+
+import ../../src/nimraylib_now/raylib
+
+##  Initialization
+## --------------------------------------------------------------------------------------
+var screenWidth: int32 = 800
+var screenHeight: int32 = 450
+initWindow(screenWidth, screenHeight, "raylib [core] example - scissor test")
+var scissorArea: Rectangle = (0f, 0f, 300f, 300f)
+var scissorMode: bool = true
+setTargetFPS(60)
+##  Set our game to run at 60 frames-per-second
+## --------------------------------------------------------------------------------------
+##  Main game loop
+while not windowShouldClose(): ##  Detect window close button or ESC key
+  ##  Update
+  ## ----------------------------------------------------------------------------------
+  if isKeyPressed(S):
+    scissorMode = not scissorMode
+  scissorArea.x = getMouseX().float32 - scissorArea.width / 2
+  scissorArea.y = getMouseY().float32 - scissorArea.height / 2
+  ## ----------------------------------------------------------------------------------
+  ##  Draw
+  ## ----------------------------------------------------------------------------------
+  beginDrawing()
+  clearBackground(Raywhite)
+  if scissorMode:
+    beginScissorMode(scissorArea.x.int32, scissorArea.y.int32, scissorArea.width.int32,
+                     scissorArea.height.int32)
+  drawRectangle(0, 0, getScreenWidth(), getScreenHeight(), Red)
+  drawText("Move the mouse around to reveal this text!", 190, 200, 20, Lightgray)
+  if scissorMode:
+    endScissorMode()
+  drawRectangleLinesEx(scissorArea, 1, Black)
+  drawText("Press S to toggle scissor test", 10, 10, 20, Black)
+  endDrawing()
+## ----------------------------------------------------------------------------------
+##  De-Initialization
+## --------------------------------------------------------------------------------------
+closeWindow()
+##  Close window and OpenGL context
+## --------------------------------------------------------------------------------------
