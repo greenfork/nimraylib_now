3e898490b7df03ab7aa0d545f30c15682c470657
Dmitry Matveyev <dm.matveyev@protonmail.com>
2021-01-17 20:19:40 +0600

    Add test authorship

--- examples/textures/textures_background_scrolling.nim
+++ examples/textures/textures_background_scrolling.nim
@@ -1,13 +1,14 @@
-## ******************************************************************************************
-##
-##    raylib [textures] example - Background scrolling
-##
-##    This example has been created using raylib 2.0 (www.raylib.com)
-##    raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-##
-##    Copyright (c) 2019 Ramon Santamaria (@raysan5)
-##
-## ******************************************************************************************
+# ******************************************************************************************
+#
+#    raylib [textures] example - Background scrolling
+#
+#    This example has been created using raylib 2.0 (www.raylib.com)
+#    raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+#
+#    Copyright (c) 2019 Ramon Santamaria (@raysan5)
+#    Converted in 2021 by greenfork
+#
+# ******************************************************************************************
 
 import lenientops
 import ../../src/nimraylib_now/raylib
