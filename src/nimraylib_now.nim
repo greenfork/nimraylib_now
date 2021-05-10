fbba56d940d3f3165917ca5279bc68d3e41b598e
Dmitry Matveyev <greenfork.lists@yandex.com>
2021-05-10 14:59:11 +0600

    Change import names

--- src/nimraylib_now.nim
+++ src/nimraylib_now.nim
@@ -4,10 +4,10 @@ import nimraylib_now/raylib
 import nimraylib_now/raymath
 import nimraylib_now/physac
 import nimraylib_now/raygui
-import nimraylib_now/conv
+import nimraylib_now/converters
 
 export raylib
 export raymath
 export physac
 export raygui
-export conv
+export converters
