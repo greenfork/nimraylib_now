36153cf17a5e057da2319955233645d06e37933f
Dmitry Matveyev <greenfork.lists@yandex.com>
2021-05-22 14:14:52 +0600

    It works

--- examples/models/models_mesh_generation.nim
+++ examples/models/models_mesh_generation.nim
@@ -10,31 +10,29 @@
 *
 ********************************************************************************************]#
 
-import nimraylib_now
+import ../../src/nimraylib_now
 
 var camera = Camera3D()
-var texture:Texture2D
-var models:seq[Model]
+var texture: Texture2D
+var models: seq[Model]
 
-func allocateMeshData(mesh:var Mesh, triangleCount:int) =
-    mesh.vertexCount = triangleCount * 3
-    mesh.triangleCount = triangleCount
+proc allocateMeshData(mesh: var Mesh, triangleCount: int) =
+  mesh.vertexCount = triangleCount * 3
+  mesh.triangleCount = triangleCount
 
-    mesh.vertices = cast[ptr cfloat](memAlloc(mesh.vertexCount * 3 * sizeof(float)) )
-    mesh.texcoords = cast[ptr cfloat](memAlloc(mesh.vertexCount * 2 * sizeof(float)) )
-    mesh.normals = cast[ptr cfloat](memAlloc(mesh.vertexCount * 3 * sizeof(float)) )
+  mesh.vertices = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 3 * sizeof(cfloat)))
+  mesh.texcoords = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 2 * sizeof(cfloat)))
+  mesh.normals = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 3 * sizeof(cfloat)) )
 
-
-proc makeMesh() :Mesh =
+proc makeMesh(): Mesh =
   allocateMeshData(result, 1)
 
-  var vertices = [0.cfloat,0,0, 1,0,2, 2,0,0]
-  var normals = [0.cfloat,1,0, 0,1,0, 0,1,0]
-  var texcoords = [0.cfloat,0, 0.5,1, 1,0]
-  result.vertices = cast[ptr cfloat](vertices.addr)
-  result.normals = cast[ptr cfloat](normals.addr)
-  result.texcoords = cast[ptr cfloat](texcoords.addr)
-
+  for idx, val in [0.cfloat,0,0, 1,0,2, 2,0,0]:
+    result.vertices[idx] = val
+  for idx, val in [0.cfloat,1,0, 0,1,0, 0,1,0]:
+    result.normals[idx] = val
+  for idx, val in [0.cfloat,0, 0.5,1, 1,0]:
+    result.texcoords[idx] = val
 
 proc init() =
   const
@@ -43,19 +41,26 @@ proc init() =
 
   initWindow(screenWidth, screenHeight, "raylib [models] example - mesh generation")
 
-  let meshes = [ genMeshPlane(2,2,5,5), genMeshCube(2.0f, 1.0f, 2.0f), genMeshSphere(2, 32, 32),
-                 genMeshHemiSphere(2, 16, 16), genMeshCylinder(1, 2, 16), genMeshTorus(0.25f, 4.0f, 16, 32),
-                 genMeshKnot(1.0f, 2.0f, 16, 128), genMeshPoly(5, 2.0f), makeMesh() ]
+  let meshes = [
+    genMeshPlane(2,2,5,5),
+    genMeshCube(2.0f, 1.0f, 2.0f),
+    genMeshSphere(2, 32, 32),
+    genMeshHemiSphere(2, 16, 16),
+    genMeshCylinder(1, 2, 16),
+    genMeshTorus(0.25f, 4.0f, 16, 32),
+    genMeshKnot(1.0f, 2.0f, 16, 128),
+    genMeshPoly(5, 2.0f),
+    makeMesh()
+  ]
 
   # We generate a checked image for texturing
-  var checked:Image = genImageChecked(2, 2, 1, 1, RED, GREEN)
+  var checked: Image = genImageChecked(2, 2, 1, 1, Red, Green)
   texture = loadTextureFromImage(checked)
   unloadImage(checked)
 
   for mesh in meshes:
     var m = loadModelFromMesh(mesh)
-    #m.materials[0].maps[ALBEDO].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
-    m.materials[0].maps[0].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
+    m.materials[0].maps[MaterialMapIndex.Albedo.int].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
     models.add(m)
 
   camera.position = (5.0, 5.0, 5.0)
@@ -65,7 +70,6 @@ proc init() =
   camera.projection = Perspective
   camera.setCameraMode(Orbital)
   setTargetFPS(60)
-
 
 proc run() =
   var currentModel = 0
@@ -80,19 +84,18 @@ proc run() =
       if currentModel < 0: currentModel = models.len - 1
 
     beginDrawing:
-      clearBackground(WHITE)
+      clearBackground(White)
       camera.beginMode3D:
-        drawModel(models[currentModel], position, 1.0, WHITE)
+        drawModel(models[currentModel], position, 1.0, White)
         drawGrid(10, 1.0)
 
-      drawRectangle(30, 400, 310, 30, fade(SKYBLUE, 0.5f))
-      drawRectangleLines(30, 400, 310, 30, fade(DARKBLUE, 0.5f))
-      drawText("MOUSE LEFT BUTTON to CYCLE PROCEDURAL MODELS", 40, 410, 10, BLUE)
+      drawRectangle(30, 400, 310, 30, fade(SkyBlue, 0.5f))
+      drawRectangleLines(30, 400, 310, 30, fade(DarkBlue, 0.5f))
+      drawText("MOUSE LEFT BUTTON to CYCLE PROCEDURAL MODELS", 40, 410, 10, Blue)
 
       let listeModels = [ ("PLANE",680), ("CUBE",680), ("SPHERE",680), ("HEMISPHERE",640), ("CYLINDER",680),
                           ("TORUS",680), ("KNOT",680), ("POLY",680), ("Parametric(custom)",580) ]
-      drawText(listeModels[currentModel][0], listeModels[currentModel][1], 10, 20, DARKBLUE)
-
+      drawText(listeModels[currentModel][0], listeModels[currentModel][1], 10, 20, DarkBlue)
 
 proc exit() =
   unloadTexture(texture)
