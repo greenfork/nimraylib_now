80a84304c05c06c2d8b877a7e4a544d76ea519e4
jseb <git@finiderire.com>
2021-05-23 11:01:24 +0200

    added corrections of Greenfork

--- examples/models/models_mesh_generation.nim
+++ examples/models/models_mesh_generation.nim
@@ -12,97 +12,114 @@
 
 import nimraylib_now
 
-var camera = Camera3D()
-var texture:Texture2D
-var models:seq[Model]
+proc allocateMeshData(mesh: var Mesh, triangleCount: int) =
+  mesh.vertexCount = triangleCount * 3
+  mesh.triangleCount = triangleCount
 
-func allocateMeshData(mesh:var Mesh, triangleCount:int) =
-    mesh.vertexCount = triangleCount * 3
-    mesh.triangleCount = triangleCount
+  mesh.vertices = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 3 * sizeof(cfloat)))
+  mesh.texcoords = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 2 * sizeof(cfloat)))
+  mesh.normals = cast[ptr UncheckedArray[cfloat]](alloc0(mesh.vertexCount * 3 * sizeof(cfloat)))
 
-    mesh.vertices = cast[ptr cfloat](memAlloc(mesh.vertexCount * 3 * sizeof(float)) )
-    mesh.normals = cast[ptr cfloat](memAlloc(mesh.vertexCount * 3 * sizeof(float)) )
-    mesh.texcoords = cast[ptr cfloat](memAlloc(mesh.vertexCount * 2 * sizeof(float)) )
-
-
-proc makeMesh() :Mesh =
+proc makeMesh(): Mesh =
   allocateMeshData(result, 1)
 
-  var vertices = [0.cfloat,0,0, 1,0,2, 2,0,0]
-  var normals = [0.cfloat,1,0, 0,1,0, 0,1,0]
-  var texcoords = [0.cfloat,0, 0.5,1, 1,0]
-  result.vertices = cast[ptr cfloat](vertices.addr)
-  result.normals = cast[ptr cfloat](normals.addr)
-  result.texcoords = cast[ptr cfloat](texcoords.addr)
+  for idx, val in [0.cfloat,0,0, 1,0,2, 2,0,0]:
+    result.vertices[idx] = val
+  for idx, val in [0.cfloat,1,0, 0,1,0, 0,1,0]:
+    result.normals[idx] = val
+  for idx, val in [0.cfloat,0, 0.5,1, 1,0]:
+    result.texcoords[idx] = val
 
-proc init() =
+  uploadMesh(result.addr, false)
+
+proc main() =
+  # Initialization
+  #--------------------------------------------------------------------------------------
   const
     screenWidth = 800
     screenHeight = 450
 
   initWindow(screenWidth, screenHeight, "raylib [models] example - mesh generation")
 
-  let meshes = [ genMeshPlane(2,2,5,5), genMeshCube(2.0f, 1.0f, 2.0f), genMeshSphere(2, 32, 32),
-                 genMeshHemiSphere(2, 16, 16), genMeshCylinder(1, 2, 16), genMeshTorus(0.25f, 4.0f, 16, 32),
-                 genMeshKnot(1.0f, 2.0f, 16, 128), genMeshPoly(5, 2.0f), makeMesh() ]
+  let meshes = [
+    genMeshPlane(2,2,5,5),
+    genMeshCube(2.0f, 1.0f, 2.0f),
+    genMeshSphere(2, 32, 32),
+    genMeshHemiSphere(2, 16, 16),
+    genMeshCylinder(1, 2, 16),
+    genMeshTorus(0.25f, 4.0f, 16, 32),
+    genMeshKnot(1.0f, 2.0f, 16, 128),
+    genMeshPoly(5, 2.0f),
+    makeMesh()
+  ]
 
   # We generate a checked image for texturing
-  var checked:Image = genImageChecked(2, 2, 1, 1, RED, GREEN)
-  texture = loadTextureFromImage(checked)
+  var checked: Image = genImageChecked(2, 2, 1, 1, Red, Green)
+  var texture: Texture2D = loadTextureFromImage(checked)
   unloadImage(checked)
 
+  # Set checked texture as default diffuse component for all models material
+  var models: seq[Model]
   for mesh in meshes:
     var m = loadModelFromMesh(mesh)
-    #m.materials[0].maps[ALBEDO].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
-    m.materials[0].maps[0].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
+    m.materials[0].maps[MaterialMapIndex.Albedo.int].texture = texture # MATERIAL_MAP_DIFFUSE is now ALBEDO
     models.add(m)
 
-  camera.position = (5.0, 5.0, 5.0)
-  camera.target = (0.0, 0.0, 0.0)
-  camera.up = (0.0, 1.0, 0.0)
-  camera.fovy = 45.0
-  camera.projection = Perspective
-  camera.setCameraMode(Orbital)
-  setTargetFPS(60)
+  # Define the camera to look into our 3d world
+  var camera = Camera3D(
+    position: (5.0, 5.0, 5.0),
+    target: (0.0, 0.0, 0.0),
+    up: (0.0, 1.0, 0.0),
+    fovy: 45.0,
+    projection: Perspective
+  )
 
-
-proc run() =
+  var position = Vector3(x: 0.0, y: 0.0, z: 0.0)
   var currentModel = 0
-  var position = Vector3(x:0.0, y:0.0, z:0.0)
 
-  while not windowShouldClose():
-    updateCamera(camera.addr)
+  camera.setCameraMode(Orbital)
+
+  setTargetFPS(60)
+  #--------------------------------------------------------------------------------------
+
+  # Main game loop
+  while not windowShouldClose():  # Detect window close button or ESC key
+
+    # Update
+    #----------------------------------------------------------------------------------
+    updateCamera(camera.addr)  # Update internal camera and our camera
     if isMouseButtonPressed(LEFT_BUTTON) or isKeyPressed(RIGHT):
-      currentModel = (currentModel + 1) mod models.len
+      currentModel = (currentModel + 1) mod models.len  # Cycle between the textures
     if isKeyPressed(LEFT):
       currentModel = currentModel - 1
       if currentModel < 0: currentModel = models.len - 1
+    #--------------------------------------------------------------------------------------
 
+    # Draw
+    #----------------------------------------------------------------------------------
     beginDrawing:
-      clearBackground(WHITE)
+      clearBackground(White)
       camera.beginMode3D:
-        drawModel(models[currentModel], position, 1.0, WHITE)
+        drawModel(models[currentModel], position, 1.0, White)
         drawGrid(10, 1.0)
 
-      drawRectangle(30, 400, 310, 30, fade(SKYBLUE, 0.5f))
-      drawRectangleLines(30, 400, 310, 30, fade(DARKBLUE, 0.5f))
-      drawText("MOUSE LEFT BUTTON to CYCLE PROCEDURAL MODELS", 40, 410, 10, BLUE)
+      drawRectangle(30, 400, 310, 30, fade(SkyBlue, 0.5f))
+      drawRectangleLines(30, 400, 310, 30, fade(DarkBlue, 0.5f))
+      drawText("MOUSE LEFT BUTTON to CYCLE PROCEDURAL MODELS", 40, 410, 10, Blue)
 
-      let listeModels = [ ("PLANE",680), ("CUBE",680), ("SPHERE",680), ("HEMISPHERE",640), ("CYLINDER",680),
-                          ("TORUS",680), ("KNOT",680), ("POLY",680), ("Parametric(custom)",580) ]
-      drawText(listeModels[currentModel][0], listeModels[currentModel][1], 10, 20, DARKBLUE)
+      let listModels = [("PLANE",680), ("CUBE",680), ("SPHERE",680), ("HEMISPHERE",640), ("CYLINDER",680),
+                          ("TORUS",680), ("KNOT",680), ("POLY",680), ("Parametric(custom)",580)]
+      drawText(listModels[currentModel][0], listModels[currentModel][1], 10, 20, DarkBlue)
+    #--------------------------------------------------------------------------------------
 
-
-proc exit() =
+  # De-Initialization
+  #--------------------------------------------------------------------------------------
   unloadTexture(texture)
 
-  for i,m in models.pairs:
-    echo i,m
-    unloadModel(m)
+  # Unload models data (GPU VRAM)
+  for m in models: unloadModel(m)
 
-  closeWindow()
+  closeWindow()  # Close window and OpenGL context
+  #--------------------------------------------------------------------------------------
 
-
-init()
-run()
-exit()
+main()
