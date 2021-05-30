2359f40bb570854fe0b3fdd5a106059d3d1562d9
jseb <git@finiderire.com>
2021-05-30 11:27:57 +0200

    revert find_missing_examples.nim and .gitignore

---
+++ scripts/find_missing_examples.nim
@@ -0,0 +1,16 @@
+import os
+
+const
+  projectDir = currentSourcePath().parentDir().parentDir()
+  originalRaylibExamplesDir = projectDir/"raylib"/"examples"
+  nimraylibNowExamplesDir = projectDir/"examples"
+
+for dir in walkDirs(originalRaylibExamplesDir/"*"):
+  for originalExample in walkFiles(dir/"*"):
+    let (_, name, ext) = splitFile(originalExample)
+    if ext == ".c":
+      let
+        category = originalExample.parentDir.lastPathPart
+        nimExampleName = name & ".nim"
+      if not fileExists(nimraylibNowExamplesDir/category/nimExampleName):
+        echo "Missing: ", category/nimExampleName
